2807eb7c03d319945bbd6524873fb01d05a93949
Victor Fonseca <victorcel@hotmail.com>
2014-09-27 23:51:06 -0300

    Adicionei facePost que contem a classe Face que deve ler o token do usuario arquivo token e postar uma menssagem na primeira pagina que ele adminsitra, falta implementar leitura do arquivo

---
+++ facePost/facePost.php
@@ -0,0 +1,28 @@
+<?hh
+$currentDir = dirname(__FILE__); 
+require_once $currentDir.'/../includes/facebook/autoload.php';
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+
+class Face
+{
+	private function getUserToken():string
+	{
+		return "abc";
+	}
+	public function postToCodingContests(string $link, string $message) : void
+	{
+		FacebookSession::setDefaultApplication('1500260360223545', 'ca5882a5a5ce92ce25d474e5ab784dd1');
+		$session = new FacebookSession(getUserToken());
+		$request = new FacebookRequest($session, 'GET', '/me?fields=accounts');
+		$response = $request->execute();
+		$graphObject = $response->getGraphObject();
+		$pageAccessToken = $graphObject->getProperty('accounts')->getProperty('data')->getProperty('0')->getProperty('access_token');
+		var_dump($pageAccessToken);
+		$pageSessionn = new FacebookSession($pageAccessToken);
+		$msg = array('link' => $link,
+			'message' => $message);
+		$request = new FacebookRequest($pageSessionn, 'POST', '/me/feed', $msg);
+		$request->execute();
+	}
+}
